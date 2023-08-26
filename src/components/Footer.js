3601c1bbad6215f672a7235f916a6d806f5cafc2
Denis Murphy <ashwinchhetri457@gmail.com>
2023-08-26 20:14:06 +0530

    updated_links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,17 +6,17 @@ const Footer = () => {
     return (
         <footer className="footer">
             <div className="footer-links">
-                <a>Home</a>
-                <a>About Us</a>
-                <a>Services</a>
-                <a>Portfolio</a>
-                <a>Contact</a>
+                <a href="/Attendance_Management/">Home</a>
+                <a href="https://ashwinchhetri.github.io/Webs_res/">About Us</a>
+                <a href="https://github.com/Ashwinchhetri?tab=repositories">Services</a>
+                <a href="https://ashwinchhetri.github.io/Webs_res/">Portfolio</a>
+                <a href="https://ashwinchhetri.github.io/Webs_res/">Contact</a>
             </div>
             <div className="footer-social">
-                <a className="social-icon"><FaFacebook /></a>
-                <a className="social-icon"><FaTwitter /></a>
-                <a className="social-icon"><FaLinkedin /></a>
-                <a className="social-icon"><FaInstagram /></a>
+                <a href="https://www.linkedin.com/in/ashwin-chhetri-b76924223/" className="social-icon"><FaLinkedin /></a>
+                <a href="https://www.facebook.com/" className="social-icon"><FaFacebook /></a>
+                <a href="https://twitter.com/" className="social-icon"><FaTwitter /></a>
+                <a href="https://www.instagram.com/" className="social-icon"><FaInstagram /></a>
             </div>
             <p className="footer-text">
                 &copy; 2023 Ashwin Chhetri. All Rights Reserved.
